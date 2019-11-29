5e731dd53ba0179cbba00e6bbbe61d7be41c41f7
Ahmed El-Mhadi <agelmahdi@gmail.com>
2019-11-29 14:32:18 +0200

    Initial Commit

---
+++ SwiftUiFetchList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiFetchList
+//
+//  Created by Ahmed El-Mahdi on 11/29/19.
+//  Copyright © 2019 Ahmed El-Mahdi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
